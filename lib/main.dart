20308eddbc7037c8fdbea0d2a6e2f14c09f5b29e
Mohamed Farahat <mofarahat96@outlook.copm>
2025-06-30 06:07:26 +0300

    project init

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(const  Bookly())
+}
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+  @override
+    Widget build(BuildContext context) {
+    return GetMaterialApp(
+    }
