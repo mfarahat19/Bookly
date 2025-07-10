a5d51b5263aa27dd4f867eb9fdd7aa2529716dcd
Mohamed Farahat <mofarahat88@gmail.com>
2025-07-10 20:53:09 +0300

    add GetIt

---
+++ lib/core/utils/servicers_locator.dart
@@ -0,0 +1,18 @@
+import 'package:bookly/core/utils/services/api_services.dart';
+import 'package:bookly/features/home/data/repos/home_repo_impl.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+void setupServicesLocator() {
+  getIt.registerSingleton<HomeRepoImpl>(
+    HomeRepoImpl(
+      getIt.get<ApiServices>(),
+    ),
+  );
+  getIt.registerSingleton<ApiServices>(
+    ApiServices(
+      Dio(),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
-import 'package:bookly/config/router_manager.dart';
+import 'package:bookly/config/app.dart';
+import 'package:bookly/core/utils/servicers_locator.dart';
+import 'package:bookly/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly/features/home/presentation/view%20model/featured_book_cubit/featured_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'features/home/presentation/view model/newest_book_cubit/newest_books_cubit.dart';
 
 void main() {
-  runApp(const Bookly());
+  setupServicesLocator();
+  runApp(MultiBlocProvider(providers: [
+    BlocProvider(
+      create: (context) => FeaturedBooksCubit(
+        getIt.get<HomeRepoImpl>(),
+      ),
+    ),
+    BlocProvider(
+      create: (context) => NewestBooksCubit(
+        getIt.get<HomeRepoImpl>(),
+      ),
+    )
+  ], child: const Bookly()));
 }
 
-class Bookly extends StatelessWidget {
-  const Bookly({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: RouterManager.router,
-      theme: ThemeData.dark(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
